8b042bee4a9c1d684db9ee57e3c7f8d6ecfea7b2
root <root@ip-172-31-1-115.ap-south-1.compute.internal>
2025-09-18 04:58:07 +0000

    Updated files from EC2

--- test_app.py
+++ test_app.py
@@ -4,10 +4,14 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello from Flask App!" in response.data
+    assert b"My Flask Web App" in response.data  # Check HTML content
 
 def test_add():
     tester = app.test_client()
     response = tester.get('/add/5/3')
     assert response.status_code == 200
     assert b"8" in response.data
+
+
+
+
